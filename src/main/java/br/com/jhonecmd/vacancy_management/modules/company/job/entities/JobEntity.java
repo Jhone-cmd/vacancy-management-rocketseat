203e0cffaddb6a5628e990f8fc648acf71521b42
Jhone-cmd <jhone.1941@gmail.com>
2026-02-06 14:45:59 -0300

    feat: creating job entity

---
+++ src/main/java/br/com/jhonecmd/vacancy_management/modules/company/job/entities/JobEntity.java
@@ -0,0 +1,45 @@
+package br.com.jhonecmd.vacancy_management.modules.company.job.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import br.com.jhonecmd.vacancy_management.modules.company.entities.CompanyEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name = "jobs")
+public class JobEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String benefits;
+    private Level level;
+
+    @ManyToOne()
+    @JoinColumn(name = "company_id", insertable = false, updatable = false)
+    private CompanyEntity companyEntity;
+
+    @Column(name = "company_id")
+    private UUID companyId;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    private enum Level {
+        JUNIOR, Pleno, Senior;
+    }
+}
