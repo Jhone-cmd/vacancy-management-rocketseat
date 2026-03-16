c42e17c7bb7fc482eb763582eff690d5e0003919
Jhone-cmd <jhone.1941@gmail.com>
2026-03-16 08:44:48 -0300

    creating the first test in Java

---
+++ src/test/java/br/com/jhonecmd/vacancy_management/FirstTest.java
@@ -0,0 +1,20 @@
+package br.com.jhonecmd.vacancy_management;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    @DisplayName("should be able possible to calculate two numbers")
+    public void should_be_able_possible_to_calculate_two_numbers() {
+        var result = calculate(3, 2);
+        assertEquals(result, 5);
+    }
+
+    public static int calculate(int num1, int num2) {
+        return num1 + num2;
+    }
+}
