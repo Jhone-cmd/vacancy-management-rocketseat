ab25156ecc55fe640e05279f9eefa39cb0f6f290
Jhone-cmd <jhone.1941@gmail.com>
2026-03-16 09:08:01 -0300

    test validating if the Job does not exist.

--- src/test/java/br/com/jhonecmd/vacancy_management/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/jhonecmd/vacancy_management/ApplyJobCandidateUseCaseTest.java
@@ -8,8 +8,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
 
 import br.com.jhonecmd.vacancy_management.exceptions.CandidateNotFound;
+import br.com.jhonecmd.vacancy_management.exceptions.JobNotFound;
+import br.com.jhonecmd.vacancy_management.modules.candidate.entities.CandidateEntity;
 import br.com.jhonecmd.vacancy_management.modules.candidate.repositories.CandidateRepository;
 import br.com.jhonecmd.vacancy_management.modules.candidate.useCases.ApplyJobCandidateUseCase;
 import br.com.jhonecmd.vacancy_management.modules.company.job.repositories.JobRepository;
@@ -33,6 +39,24 @@ public class ApplyJobCandidateUseCaseTest {
             this.applyJobCandidateUseCase.execute(null, null);
         } catch (Exception ex) {
             assertThat(ex).isInstanceOf(CandidateNotFound.class);
+        }
+    }
+
+    @Test
+    @DisplayName("Should not be able to apply job with job not found.")
+    public void should_not_be_able_to_apply_job_with_job_not_found() {
+        try {
+
+            var candidateId = UUID.randomUUID();
+            var candidate = new CandidateEntity();
+            candidate.setId(candidateId);
+
+            when(this.candidateRepository.findById(candidateId)).thenReturn(Optional.of(candidate));
+
+            this.applyJobCandidateUseCase.execute(candidateId, null);
+
+        } catch (Exception ex) {
+            assertThat(ex).isInstanceOf(JobNotFound.class);
         }
     }
 }
