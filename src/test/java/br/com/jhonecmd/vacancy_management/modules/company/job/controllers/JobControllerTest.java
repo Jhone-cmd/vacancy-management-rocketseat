0aac26ed76ac1cdc932137ff29d348bfb8030b85
Jhone-cmd <jhone.1941@gmail.com>
2026-03-17 10:20:10 -0300

    JobController: inserting database into memory for testing

--- src/test/java/br/com/jhonecmd/vacancy_management/modules/company/job/controllers/JobControllerTest.java
+++ src/test/java/br/com/jhonecmd/vacancy_management/modules/company/job/controllers/JobControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,45 +19,60 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import br.com.jhonecmd.vacancy_management.modules.company.entities.CompanyEntity;
 import br.com.jhonecmd.vacancy_management.modules.company.job.dto.CreateJobDTO;
+import br.com.jhonecmd.vacancy_management.modules.company.repositories.CompanyRepository;
 import br.com.jhonecmd.vacancy_management.utils.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 // @AutoConfigureMockMvc
 public class JobControllerTest {
 
-    @Value("${security.token.secret}")
-    private String secret;
+        @Value("${security.token.secret}")
+        private String secret;
 
-    private MockMvc mvc;
+        private MockMvc mvc;
 
-    @Autowired
-    private WebApplicationContext context;
+        @Autowired
+        private CompanyRepository companyRepository;
 
-    @BeforeEach
-    public void setup() {
-        mvc = MockMvcBuilders.webAppContextSetup(context)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
-    }
+        @Autowired
+        private WebApplicationContext context;
 
-    @Test
-    @DisplayName("Should be able to create a new job.")
-    public void should_be_able_to_create_a_new_job() throws Exception {
+        @BeforeEach
+        public void setup() {
+                mvc = MockMvcBuilders.webAppContextSetup(context)
+                                .apply(SecurityMockMvcConfigurers.springSecurity())
+                                .build();
+        }
 
-        var createdJobDTO = CreateJobDTO.builder().name("java test-" + UUID.randomUUID()).benefits("benefits test")
-                .description("description test").level("junior").build();
+        @Test
+        @DisplayName("Should be able to create a new job.")
+        public void should_be_able_to_create_a_new_job() throws Exception {
 
-        var result = mvc.perform(MockMvcRequestBuilders.post("/companies/jobs")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(TestUtils.objectToJson(createdJobDTO))
-                .header("Authorization",
-                        TestUtils.generateToken(UUID.fromString("9a91f4c0-9c4f-47d8-bdce-204498b6063b"), secret)))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+                var company = CompanyEntity.builder().name("Company-test").email("company@email.com")
+                                .password("1234567890").description("description test")
+                                .webSite("https://www.company.org").build();
 
-        System.out.println(result);
-    }
+                company = this.companyRepository.saveAndFlush(company);
+
+                var createdJobDTO = CreateJobDTO.builder().name("java test-" + UUID.randomUUID())
+                                .benefits("benefits test")
+                                .description("description test").level("junior").build();
+
+                var result = mvc.perform(MockMvcRequestBuilders.post("/companies/jobs")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(TestUtils.objectToJson(createdJobDTO))
+                                .header("Authorization",
+                                                TestUtils.generateToken(
+                                                                company.getId(),
+                                                                secret)))
+                                .andExpect(MockMvcResultMatchers.status().isCreated());
+
+                System.out.println(result);
+        }
 
 }
