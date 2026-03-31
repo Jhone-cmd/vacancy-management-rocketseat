c1897d08926589cd3556fb7dd9a691e05bb899b0
Jhone-cmd <jhone.1941@gmail.com>
2026-03-31 09:37:30 -0300

    hiding the redirect route.

--- src/main/java/br/com/jhonecmd/vacancy_management/controllers/RedirectController.java
+++ src/main/java/br/com/jhonecmd/vacancy_management/controllers/RedirectController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
 @RestController
 @RequestMapping("/")
 public class RedirectController {
+
+    @Hidden
     @GetMapping("")
     public RedirectView redirectToSwagger() {
         return new RedirectView("/swagger-ui/index.html");
